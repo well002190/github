bb6358336f8fdaa685a563664094950ad5e8d589
黃瑋儒 <83324693+well002190@users.noreply.github.com>
2021-04-28 11:16:29 +0800

    Update Form1.Designer.cs

--- github/Form1.Designer.cs
+++ github/Form1.Designer.cs
@@ -56,7 +56,7 @@ namespace github
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "GITHUB改的";
+            this.button1.Text = "GITHUB改的2";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
