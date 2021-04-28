0724a21cf1b6fbebf86908d380819abfbd607ff9
瑋儒 黃 <well002190@gmail.com>
2021-04-28 11:08:08 +0800

    新增TEXTBOX

--- github/Form1.Designer.cs
+++ github/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace github
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,11 +41,20 @@ namespace github
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "我要上傳github";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "新增的TEXTBOX";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +66,7 @@ namespace github
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
