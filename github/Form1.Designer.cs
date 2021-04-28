c7ee87dde862efd012800fbbafcc2352060c6155
瑋儒 黃 <well002190@gmail.com>
2021-04-28 11:29:44 +0800

    hahaha

--- github/Form1.Designer.cs
+++ github/Form1.Designer.cs
@@ -34,6 +34,17 @@ namespace github
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -79,11 +90,121 @@ namespace github
             this.button3.Text = "GITHUB改的分支";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(328, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "GITHUB改的分支";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(336, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "GITHUB改的分支";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(344, 238);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "GITHUB改的分支";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(352, 246);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "GITHUB改的分支";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(360, 254);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "GITHUB改的分支";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(368, 262);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(160, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "GITHUB改的分支";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(376, 270);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(160, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "GITHUB改的分支";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(384, 278);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(160, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "GITHUB改的分支";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(392, 286);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(160, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "GITHUB改的分支";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(400, 294);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(160, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "GITHUB改的分支";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(408, 302);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(160, 23);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "GITHUB改的分支";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -103,6 +224,17 @@ namespace github
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
     }
 }
 
